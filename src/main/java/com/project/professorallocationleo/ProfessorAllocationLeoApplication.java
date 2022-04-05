666f7390decefb33117703e66bda923a1ad65cc5
Leandro Mendes <leandromendesjor@gmail.com>
2022-04-05 19:49:29 -0300

    Add initial SpringBoot Web project filles

---
+++ src/main/java/com/project/professorallocationleo/ProfessorAllocationLeoApplication.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationleo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAllocationLeoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAllocationLeoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/professorallocationleo/ProfessorAllocationLeoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.professorallocationleo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessorAllocationLeoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
